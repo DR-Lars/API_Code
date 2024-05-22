2ffaf26f0b59891f86f934bf76ee5583180b37d5
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-05-22 11:33:18 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.listen(port, () =>
+  console.log(`API is running on http://localhost:${port}`)
+);
+
+app.use(express.json());
+
+app.get("/station", (req, res) => {
+  res.status(200).send({
+    idWeatherStation: "4",
+    Temperature: 22,
+    Humidity: 48,
+    Moisture: 28,
+    TimeStamp: "2024-05-22 00:16:25",
+    Location: 1,
+  });
+});
+
+app.post("/station/:id", (req, res) => {
+  const { id } = req.params.id;
+  const { station } = req.body;
+
+  if (station) {
+    res.status(418).send({ message: "Please attach a JSON" });
+  } else {
+    res.status(200).send({
+      idWeatherStation: req.body.idWeatherStation,
+      Temperature: req.body.Temperature,
+      Humidity: req.body.Humidity,
+      Moisture: req.body.Moisture,
+      TimeStamp: req.body.TimeStamp,
+      Location: req.body.Location,
+    });
+  }
+});
